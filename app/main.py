2469abb901ccbea75a14743a667e16c24a4413d2
ivanshevelev <ivanshyahrd@gmail.com>
2024-05-15 00:20:32 +0300

    дописано создание таблиц

--- app/main.py
+++ app/main.py
@@ -2,12 +2,15 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from app.models.task_table import delete_tables, create_tables
 from app.routes.task_rourter import router as tasks_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Start, please wait")
+    delete_tables()
+    create_tables()
     yield
 
 
--- app/models/task_table.py
+++ app/models/task_table.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+from app.db.database import async_engine
+
 
 class Model(DeclarativeBase):
     pass
@@ -13,3 +15,14 @@ class TaskTable(Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     description: Mapped[Optional[str]]
+
+
+async def create_tables():
+    # https://docs.sqlalchemy.org/en/20/orm/extensions/asyncio.html#synopsis-core
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Model.metadata.create_all)
+
+
+async def delete_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Model.metadata.drop_all)
